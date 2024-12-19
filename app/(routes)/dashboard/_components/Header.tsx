52005b501a98ade12ee88d18e2b78939984b8af4
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2024-12-19 07:38:23 +0530

    Adding organization

--- app/(routes)/dashboard/_components/Header.tsx
+++ app/(routes)/dashboard/_components/Header.tsx
@@ -1,11 +1,18 @@
+"use client";
 import React from "react";
 import Logo from "../../../_components/Logo";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, OrganizationSwitcher, useAuth } from "@clerk/nextjs";
 
 const Header = () => {
+  const { orgId } = useAuth();
+  console.log(orgId);
   return (
     <div className="flex justify-between p-3 shadow-sm align-center">
       <Logo />
+      <OrganizationSwitcher
+        afterLeaveOrganizationUrl={"/dashboard"}
+        afterCreateOrganizationUrl={"/dashboard"}
+      />
       <UserButton />
     </div>
   );
